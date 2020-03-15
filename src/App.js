fe2fac0b278b3b77d28933f8d0d077186b8d2ab2
Sarah <sarahhargreaves.10@gmail.com>
2020-03-14 23:12:40 -0400

    Finished pop up test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     let [isDisplayed, setIsDisplayed] = React.useState(false);
+     return (
+        <div>
+            <Button onClick={() => setIsDisplayed(!isDisplayed)} > Open Popup </Button>
+            <Popup isDisplayed={isDisplayed}
+                   handleClose={setIsDisplayed}
+                   title='Popup window'>
+                this is a test
+            </Popup>
+        </div>
+     );
+}
+
+function Popup(props) {
+    return (
+        <div className='popup-overlay' style={{display: props.isDisplayed ? 'initial' : 'none'}}>
+            <Card className='popup'>
+                <Card.Header><Card.Title>{props.title}</Card.Title></Card.Header>
+                <Card.Body>{props.children}</Card.Body>
+                <Card.Footer>
+                    <Button onClick={() => props.handleClose(false)}>Close</Button>
+                </Card.Footer>
+            </Card>
+        </div>
+    );
 }
 
 export default App;
